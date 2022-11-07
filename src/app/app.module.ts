6dba6a26a13673bb5d755e685279c0a36221effb
Wessel Kuijstermans <w.kuijstermans@home.nl>
2022-11-07 12:26:32 +0100

    Working Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import { MovesComponent } from './moves/moves.component';
+import { TypesComponent } from './types/types.component';
+import { CasusComponent } from './casus/casus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    PokemonComponent,
+    MovesComponent,
+    TypesComponent,
+    CasusComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/casus/casus.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-casus',
+  templateUrl: './casus.component.html',
+  styleUrls: ['./casus.component.css']
+})
+export class CasusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
